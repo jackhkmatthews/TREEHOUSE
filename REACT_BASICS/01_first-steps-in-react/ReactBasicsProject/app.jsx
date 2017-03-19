22b15a03984d7c31bdf1b5227750da11f9e024e7
jackhkmatthews <jackhkmatthews@gmail.com>
2017-03-19 22:14:41 +0000

    communicating events

--- REACT_BASICS/01_first-steps-in-react/ReactBasicsProject/app.jsx
+++ REACT_BASICS/01_first-steps-in-react/ReactBasicsProject/app.jsx
@@ -28,6 +28,23 @@ Header.propTypes = {
   title: React.PropTypes.string.isRequired,
 }
 
+function Counter(props){
+  return (
+    <div className="counter">
+      <button className="counter-action decrement" onClick={function(){ props.onChange(-1);}}> - </button>
+      <div className="counter-score">
+        {props.score}
+      </div>
+      <button className="counter-action increment" onClick={function(){ props.onChange(1);}}> + </button>
+    </div>
+  )
+}
+
+Counter.propTypes = {
+  score: React.PropTypes.number.isRequired,
+  onChange: React.PropTypes.func.isRequired
+}
+
 function Player(props){
   return (
     <div className="player">
@@ -35,7 +52,7 @@ function Player(props){
         {props.name}
       </div>
       <div className="player-score">
-        <Counter initialScore={props.score}/>
+        <Counter score={props.score} onChange={props.onScoreChange}/>
       </div>
     </div>
   );
@@ -43,69 +60,56 @@ function Player(props){
 
 Player.propTypes = {
   name: React.PropTypes.string.isRequired,
-  score: React.PropTypes.number.isRequired
+  score: React.PropTypes.number.isRequired,
+  onScoreChange: React.PropTypes.func.isRequired
 }
 
-var Counter = React.createClass({
+var Application = React.createClass({
   propTypes: {
-    initialScore: React.PropTypes.number.isRequired
+    title: React.PropTypes.string,
+    initialPlayers: React.PropTypes.arrayOf(React.PropTypes.shape({
+      name: React.PropTypes.string.isRequired,
+      score: React.PropTypes.number.isRequired,
+      id: React.PropTypes.number.isRequired
+    })).isRequired
+  },
+
+  getDefaultProps: function(){
+    return {
+      title: "Scoreboard"
+    }
   },
 
   getInitialState: function(){
     return {
-      score: this.props.initialScore,
+      players: this.props.initialPlayers
     }
   },
 
-  incrementScore: function(e){
-    this.setState({
-      score: this.state.score + 1
-    })
-  },
-
-  decrementScore: function(e){
-    this.setState({
-      score: this.state.score - 1
-    })
+  onScoreChange: function(index, delta){
+    console.log('oScoreChange', index, delta);
+    this.state.players[index].score += delta;
+    this.setState(this.state);
   },
 
   render: function(){
     return (
-      <div className="counter">
-        <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
-        <div className="counter-score">
-          {this.state.score}
+      <div className="scoreboard">
+        <Header title={this.props.title}/>
+        <div className="players">
+          {this.state.players.map(function(player, index){
+            return (
+              <Player
+                onScoreChange={function(delta){this.onScoreChange(index, delta)}.bind(this)}
+                name={player.name}
+                score={player.score}
+                key={player.id} />
+            )
+          }.bind(this))}
         </div>
-        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
       </div>
-    )
+    );
   }
 })
 
-function Application(props){
-  return (
-    <div className="scoreboard">
-      <Header title={props.title}/>
-      <div className="players">
-        {props.players.map(function(player){
-          return <Player name={player.name} score={player.score} key={player.id} />
-        })}
-      </div>
-    </div>
-  );
-}
-
-Application.propTypes = {
-  title: React.PropTypes.string,
-  players: React.PropTypes.arrayOf(React.PropTypes.shape({
-    name: React.PropTypes.string.isRequired,
-    score: React.PropTypes.number.isRequired,
-    id: React.PropTypes.number.isRequired
-  })).isRequired
-};
-
-Application.defaultProps = {
-  title: "Scoreboard"
-}
-
-ReactDOM.render(<Application players={PLAYERS}/>, document.getElementById('container'));
+ReactDOM.render(<Application initialPlayers={PLAYERS}/>, document.getElementById('container'));
