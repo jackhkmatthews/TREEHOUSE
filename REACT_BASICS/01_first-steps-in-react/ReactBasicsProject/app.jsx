a5a59a89f2c56e653449e7d10b26068835cd684c
jackhkmatthews <jackhkmatthews@gmail.com>
2017-03-20 12:40:19 +0000

    adding a removing players from the scoreboard

--- REACT_BASICS/01_first-steps-in-react/ReactBasicsProject/app.jsx
+++ REACT_BASICS/01_first-steps-in-react/ReactBasicsProject/app.jsx
@@ -16,9 +16,74 @@ var PLAYERS = [
   }
 ]
 
+var nextId = 4;
+
+var AddPlayerForm = React.createClass({
+
+  propTypes: {
+    onAdd: React.PropTypes.func.isRequired
+  },
+
+  getInitialState: function(){
+    return {
+      name: ""
+    }
+  },
+
+  onNameChange: function(e){
+    console.log("onNameChange", e.target.value);
+    this.setState({name: e.target.value})
+  },
+
+  onSubmit: function(e){
+    e.preventDefault();
+
+    this.props.onAdd(this.state.name);
+    this.setState({name: ""});
+  },
+
+  render: function(){
+    return (
+      <div className="add-player-form">
+        <form onSubmit={this.onSubmit}>
+          <input type="text" value={this.state.name} onChange={this.onNameChange} />
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  }
+})
+
+function Stats(props) {
+  var totalPlayers = props.players.length;
+  var totalPoints = props.players.reduce(function(total, player){
+    return total += player.score;
+  }, 0)
+
+  return (
+    <table className="stats">
+      <tbody>
+        <tr>
+          <td>Players:</td>
+          <td>{totalPlayers}</td>
+        </tr>
+        <tr>
+          <td>Total Points:</td>
+          <td>{totalPoints}</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+Stats.propTypes = {
+  players: React.PropTypes.array.isRequired
+}
+
 function Header(props){
   return (
     <div className="header">
+      <Stats players={props.players}/>
       <h1>{props.title}</h1>
     </div>
   );
@@ -26,6 +91,7 @@ function Header(props){
 
 Header.propTypes = {
   title: React.PropTypes.string.isRequired,
+  players: React.PropTypes.array.isRequired
 }
 
 function Counter(props){
@@ -49,6 +115,7 @@ function Player(props){
   return (
     <div className="player">
       <div className="player-name">
+        <a className="remove-player" onClick={props.onRemove}>x</a>
         {props.name}
       </div>
       <div className="player-score">
@@ -61,7 +128,8 @@ function Player(props){
 Player.propTypes = {
   name: React.PropTypes.string.isRequired,
   score: React.PropTypes.number.isRequired,
-  onScoreChange: React.PropTypes.func.isRequired
+  onScoreChange: React.PropTypes.func.isRequired,
+  onRemove: React.PropTypes.func.isRequired
 }
 
 var Application = React.createClass({
@@ -92,21 +160,40 @@ var Application = React.createClass({
     this.setState(this.state);
   },
 
+  onPlayerAdd: function(name){
+    console.log('player added: ', name);
+    this.state.players.push({
+      name: name,
+      score: 0,
+      id: nextId
+    });
+    this.setState(this.state);
+    nextId += 1;
+  },
+
+  onRemovePlayer: function(index){
+    this.state.players.splice(index, 1);
+    this.setState(this.state);
+    console.log('remove: ', index);
+  },
+
   render: function(){
     return (
       <div className="scoreboard">
-        <Header title={this.props.title}/>
+        <Header title={this.props.title} players={this.state.players}/>
         <div className="players">
           {this.state.players.map(function(player, index){
             return (
               <Player
                 onScoreChange={function(delta){this.onScoreChange(index, delta)}.bind(this)}
+                onRemove={function(){this.onRemovePlayer(index)}.bind(this)}
                 name={player.name}
                 score={player.score}
                 key={player.id} />
             )
           }.bind(this))}
         </div>
+        <AddPlayerForm onAdd={this.onPlayerAdd} />
       </div>
     );
   }
