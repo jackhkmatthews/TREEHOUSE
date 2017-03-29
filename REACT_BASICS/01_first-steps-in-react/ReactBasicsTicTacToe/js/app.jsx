bdf3ee7826a117bd5ee0219404b0734a5a301397
jackhkmatthews <jackhkmatthews@gmail.com>
2017-03-29 12:59:20 +0100

    application component split into header, scoreboard and cell. Cell is now a statefull component. onclick cell changes to x

--- REACT_BASICS/01_first-steps-in-react/ReactBasicsTicTacToe/js/app.jsx
+++ REACT_BASICS/01_first-steps-in-react/ReactBasicsTicTacToe/js/app.jsx
@@ -1,23 +1,70 @@
 console.log('hi');
 
-function Application(){
+const CELLS = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var Cell = React.createClass({
+  propTypes: {
+    cell: React.PropTypes.number.isRequired,
+  },
+
+  getInitialState: function(){
+    return {
+      cell: '.'
+    };
+  },
+
+  updateCell: function(){
+    this.setState({
+      cell: 'x'
+    });
+  },
+
+  render: function() {
+    return (
+      <li className="gameboard__cell" onClick={this.updateCell}>{this.state.cell}</li>
+    );
+  }
+});
+
+function Scoreboard(props){
+  return (
+    <ul  className="gameboard">
+      {props.cells.map(cell => {
+        return (
+          <Cell cell={cell} key={cell}/>
+        );
+      })}
+    </ul>
+  );
+}
+
+Scoreboard.propTypes = {
+  cells: React.PropTypes.array.isRequired
+};
+
+function Header(props){
+  return (
+    <h1 className="game__title">{props.title}</h1>
+  );
+}
+
+Header.propTypes = {
+  title: React.PropTypes.string.isRequired
+};
+
+function Application(props){
   return (
     <div className="game">
-      <h1 className="game__title">Tic Tac Toe</h1>
-      <ul  className="gameboard">
-        <li className="gameboard__cell">x</li>
-        <li className="gameboard__cell">x</li>
-        <li className="gameboard__cell">x</li>
-        <li className="gameboard__cell">x</li>
-        <li className="gameboard__cell">x</li>
-        <li className="gameboard__cell">x</li>
-        <li className="gameboard__cell">x</li>
-        <li className="gameboard__cell">x</li>
-        <li className="gameboard__cell">x</li>
-      </ul>
+      <Header title={props.title} />
+      <Scoreboard cells={props.cells}/>
       <button className="game__reset">Reset</button>
     </div>
   );
 }
 
-ReactDOM.render(<Application />, document.getElementById('container'));
+Application.propTypes = {
+  title: React.PropTypes.string.isRequired,
+  cells: React.PropTypes.array.isRequired
+};
+
+ReactDOM.render(<Application title="Tic Tac Toe" cells={CELLS}/>, document.getElementById('container'));
