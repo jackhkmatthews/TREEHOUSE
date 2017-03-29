4a535df9142f0ed6666966a65b86d78ce19a1dbe
jackhkmatthews <jackhkmatthews@gmail.com>
2017-03-29 11:50:24 +0100

    wireframe of game and gameboard in scss and jsx

--- REACT_BASICS/01_first-steps-in-react/ReactBasicsTicTacToe/js/app.jsx
+++ REACT_BASICS/01_first-steps-in-react/ReactBasicsTicTacToe/js/app.jsx
@@ -1,1 +1,23 @@
 console.log('hi');
+
+function Application(){
+  return (
+    <div className="game">
+      <h1 className="game__title">Tic Tac Toe</h1>
+      <ul  className="gameboard">
+        <li className="gameboard__cell">x</li>
+        <li className="gameboard__cell">x</li>
+        <li className="gameboard__cell">x</li>
+        <li className="gameboard__cell">x</li>
+        <li className="gameboard__cell">x</li>
+        <li className="gameboard__cell">x</li>
+        <li className="gameboard__cell">x</li>
+        <li className="gameboard__cell">x</li>
+        <li className="gameboard__cell">x</li>
+      </ul>
+      <button className="game__reset">Reset</button>
+    </div>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById('container'));
