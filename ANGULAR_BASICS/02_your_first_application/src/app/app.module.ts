849e222d16998a39f328ae756838813ffc3fb56f
Jack Matthews <jackhkmatthews@gmail.com>
2017-05-03 16:13:44 +0100

    created first module and component. Split template and css into seperate files. Bootstrapped AppComponent to AppModule/RootModule so it will run on load. Created first nested components

--- ANGULAR_BASICS/02_your_first_application/src/app/app.module.ts
+++ ANGULAR_BASICS/02_your_first_application/src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { EntryListComponent, EntryComponent } from './entries'
 
 @NgModule({
-  imports: [BrowserModule]
+  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    EntryComponent,
+    EntryListComponent
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
+  
 }
