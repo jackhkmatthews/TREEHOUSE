398ab3840ff1b3bb5f9d26a655cc5d40673c6083
Jack Matthews <jackhkmatthews@gmail.com>
2017-05-03 16:45:42 +0100

    fleshed out entry component with content copy and directives

--- ANGULAR_BASICS/02_your_first_application/src/app/entries/entry/entry.component.ts
+++ ANGULAR_BASICS/02_your_first_application/src/app/entries/entry/entry.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['entry.component.css']
 })
 export class EntryComponent {
-
+  title: string = 'My First Photo';
+  photo: string = 'http://placehold.it/800x500?text=Angular Basics';
+  description: string = 'A description of my first photo';
+  comments: any[] = [
+    {name: "John", comment: "A comment"},
+    {name: "Jack", comment: "A comment"},
+    {name: "Emma", comment: "A comment"}
+  ]
 }
