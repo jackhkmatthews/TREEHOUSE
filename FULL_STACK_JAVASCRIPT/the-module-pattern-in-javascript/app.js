160785b8509fe3a544c4fb1c43a81236d321068d
Jack Matthews <jackhkmatthews@gmail.com>
2017-05-15 18:52:48 +0100

    created module and imported into a second module

--- FULL_STACK_JAVASCRIPT/the-module-pattern-in-javascript/app.js
+++ FULL_STACK_JAVASCRIPT/the-module-pattern-in-javascript/app.js
@@ -1,3 +1,5 @@
+//////////////////////////////
+
 // !function(){
 //   function foo(){
 //     console.log('self execute');
@@ -5,17 +7,50 @@
 //   foo();
 // }();
 
-var crazyCoolLibrary = {
-  word: 'cool',
-  speak: function(){
-    console.log(this.word);
-  }
-};
+//////////////////////////////
 
-!function(object){
-  object.shout = function(string){
+// var crazyCoolLibrary = {
+//   word: 'cool',
+//   speak: function(){
+//     console.log(this.word);
+//   }
+// };
+
+// !function(object){
+//   object.shout = function(string){
+//     console.log(string.toUpperCase());
+//   };
+//   object.shout(object.word);
+//   object.speak();
+
+//   var exports = {};
+//   exports.shout = object.shout;
+//   return exports;
+// }(crazyCoolLibrary);
+
+//////////////////////////////
+
+var library = function(){
+  var exports = {};
+  
+  exports.word = 'word';
+
+  exports.shout = function(string){
     console.log(string.toUpperCase());
   };
-  object.shout(object.word);
-  object.speak();
-}(crazyCoolLibrary);
+
+  return exports;
+}();
+
+
+var app = function(library){
+  var exports = {};
+
+  exports.word = 'different word';
+
+  exports.shout = function(string){
+    library.shout(string + ' and a little extra');
+  };
+
+  return exports; 
+}(library);
