4ee205ec8920cc794017c0f0e7a8eda37f596e47
Jack Matthews <jackhkmatthews@gmail.com>
2017-05-15 18:32:51 +0100

    imported global variable or object into our module

--- FULL_STACK_JAVASCRIPT/the-module-pattern-in-javascript/app.js
+++ FULL_STACK_JAVASCRIPT/the-module-pattern-in-javascript/app.js
@@ -1,6 +1,21 @@
-!function(){
-  function foo(){
-    console.log('self execute');
+// !function(){
+//   function foo(){
+//     console.log('self execute');
+//   }
+//   foo();
+// }();
+
+var crazyCoolLibrary = {
+  word: 'cool',
+  speak: function(){
+    console.log(this.word);
   }
-  foo();
-}();
+};
+
+!function(object){
+  object.shout = function(string){
+    console.log(string.toUpperCase());
+  };
+  object.shout(object.word);
+  object.speak();
+}(crazyCoolLibrary);
